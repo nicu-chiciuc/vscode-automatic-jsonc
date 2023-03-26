aaad662fdecd91aa35a0d1c35d53c59626ec2f9a
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-03-26 19:32:57 +0300

    add test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,66 @@
-import * as assert from 'assert';
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  test("Convert package.jsonc to package.json without comments", async () => {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      return;
+    }
+    const workspacePath = workspaceFolders[0].uri.fsPath;
+    const jsoncPath = path.join(workspacePath, "package.jsonc");
+    const jsonPath = path.join(workspacePath, "package.json");
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    fs.writeFileSync(
+      jsoncPath,
+      '{\n  "name": "test-package",\n  "version": "1.0.0"\n}'
+    );
+
+    const document = await vscode.workspace.openTextDocument(jsoncPath);
+    const editor = await vscode.window.showTextDocument(document);
+
+    await editor.document.save();
+
+    const convertedContent = fs.readFileSync(jsonPath, "utf8");
+    assert.strictEqual(
+      convertedContent,
+      '{\n  "name": "test-package",\n  "version": "1.0.0"\n}'
+    );
+
+    fs.unlinkSync(jsoncPath);
+    fs.unlinkSync(jsonPath);
+  });
+
+  test("Convert package.jsonc to package.json with comments", async () => {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      return;
+    }
+    const workspacePath = workspaceFolders[0].uri.fsPath;
+    const jsoncPath = path.join(workspacePath, "package.jsonc");
+    const jsonPath = path.join(workspacePath, "package.json");
+
+    fs.writeFileSync(
+      jsoncPath,
+      '{\n  // This is a comment\n  "name": "test-package",\n  "version": "1.0.0"\n}'
+    );
+
+    const document = await vscode.workspace.openTextDocument(jsoncPath);
+    const editor = await vscode.window.showTextDocument(document);
+
+    await editor.document.save();
+
+    const convertedContent = fs.readFileSync(jsonPath, "utf8");
+    assert.strictEqual(
+      convertedContent,
+      '{\n  "name": "test-package",\n  "version": "1.0.0"\n}'
+    );
+
+    fs.unlinkSync(jsoncPath);
+    fs.unlinkSync(jsonPath);
+  });
 });
